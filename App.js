0c1dcd2bbfa30b407aaa466028a7d255c05960a2
Ovidiu Cristescu <55203625+LunatiqueCoder@users.noreply.github.com>
2025-06-09 12:17:24 +0300

    fix expo snack

--- App.js
+++ App.js
@@ -1,15 +1,1 @@
-/** 
- * @description This file is needed for Expo Router to work with Snack
- * @link https://github.com/expo/snack/issues/459#issuecomment-2821318093
- */
-
-import { ExpoRoot } from "expo-router";
-import Head from "expo-router/head";
-
-export default function ExpoRouterApp() {
-    return (
-        <Head.Provider>
-            <ExpoRoot context={require.context("./app", true)} location="/" />
-        </Head.Provider>
-    );
-}
+import 'expo-router/entry';
